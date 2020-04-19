29412f87f7e2be9debc72dde47b47ffc333d0640
Calum <CalumD3@hotmail.com>
2020-04-19 23:30:14 +0100

    Map out parseAllElements ready for parsing

--- src/main/java/Values/KeyTape.java
+++ src/main/java/Values/KeyTape.java
@@ -1,12 +1,30 @@
 package Values;
 
+import Exceptions.JSONParseException;
+import Exceptions.KeyInvalidException;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class KeyTape extends Tape<String> {
+class KeyTape extends Tape<String> {
+
+    private static final String VALID_KEY_ACCESSOR = "[ / <Object Key>";
 
     public KeyTape(String fullInput) {
         super(fullInput);
+    }
+
+    public List<String> parseAllElements() {
+        try {
+            List<String> allElements = new ArrayList<>();
+            while (currentIndex < fullInput.length()) {
+                allElements.add(parseNextElement());
+            }
+            allElements.add("");
+            return allElements;
+        } catch (JSONParseException e) {
+            throw new KeyInvalidException(e.getMessage(), e);
+        }
     }
 
     @Override
