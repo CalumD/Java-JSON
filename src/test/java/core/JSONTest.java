94649f8cd8f1a373c7f1d68d4d72e53bad30e4ee
Calum <CalumD3@hotmail.com>
2020-04-30 21:26:45 +0100

    Make JSON test an abstract to enforce which tests we write for implementing classes instead.

--- src/test/java/core/JSONTest.java
+++ src/test/java/core/JSONTest.java
@@ -1,62 +1,8 @@
 package core;
 
-import exceptions.JSONParseException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+public abstract class JSONTest {
 
-public class JSONTest {
+    public abstract void testDataType();
 
-    /**
-     * Can Ignore any implemented method, since they are only there to get an instance of a 'JSON.java'.
-     */
-    private class AbsractClass extends JSON {
 
-        AbsractClass(JSONTape parsingTape) throws JSONParseException {
-            super(parsingTape);
-        }
-
-        @Override
-        public Object getValue() {
-            return null;
-        }
-
-        @Override
-        public String asString(int depth) {
-            return null;
-        }
-
-        @Override
-        public boolean equals(Object otherJSON) {
-            return false;
-        }
-
-        @Override
-        public int hashCode() {
-            return 0;
-        }
-    }
-
-    private AbsractClass absractClass;
-    private AbsractClass booleanClass;
-    private AbsractClass stringClass;
-    private AbsractClass longClass;
-    private AbsractClass doubleClass;
-    private AbsractClass arrayClass;
-    private AbsractClass objectClass;
-
-    @BeforeEach
-    public void setup() {
-        absractClass = new AbsractClass(new JSONTape(""));
-        booleanClass = new AbsractClass(new JSONTape("true"));
-        stringClass = new AbsractClass(new JSONTape("\"hello world\""));
-        longClass = new AbsractClass(new JSONTape("1234"));
-        doubleClass = new AbsractClass(new JSONTape("1.23"));
-        arrayClass = new AbsractClass(new JSONTape("[]"));
-        objectClass = new AbsractClass(new JSONTape("{}"));
-    }
-
-    @Test
-    public void doNothingYet() {
-        System.out.println(absractClass);
-    }
 }
