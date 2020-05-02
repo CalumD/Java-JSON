f537afe47decc4d8404c10ed784d08a08213ca11
Calum <CalumD3@hotmail.com>
2020-05-02 16:04:21 +0100

    Add base case tests for all public methods in the abstract JSON class

--- src/test/java/core/JSONTest.java
+++ src/test/java/core/JSONTest.java
@@ -1,8 +1,108 @@
 package core;
 
+import org.junit.jupiter.api.Test;
+
 public abstract class JSONTest {
 
+    @Test
     public abstract void testDataType();
 
+    @Test
+    public abstract void simpleCreateFromString(); // this is actually just a pass-through to the JSONtape.
 
+    @Test
+    public abstract void simpleCreateFromMultilineString(); // this is actually just a pass-through to the JSONtape.
+
+    @Test
+    public abstract void containsTheEmptyDoubleQuoteIsTrue();
+
+    @Test
+    public abstract void containsNullIsFalse();
+
+    @Test
+    public abstract void containsAllTheseKeys();
+
+    @Test
+    public abstract void getKeys();
+
+    @Test
+    public abstract void getValues();
+
+    @Test
+    public abstract void getArray();
+
+    @Test
+    public abstract void getBoolean();
+
+    @Test
+    public abstract void getDouble();
+
+    @Test
+    public abstract void getLong();
+
+    @Test
+    public abstract void getString();
+
+    @Test
+    public abstract void getJSONObject();
+
+    @Test
+    public abstract void getAny();
+
+    @Test
+    public abstract void getValueAt();
+
+    @Test
+    public abstract void getDataTypeOf();
+
+    @Test
+    public abstract void getKeysOf();
+
+    @Test
+    public abstract void getValuesOf();
+
+    @Test
+    public abstract void getJSONObjectAt();
+
+    @Test
+    public abstract void getArrayAt();
+
+    @Test
+    public abstract void getBooleanAt();
+
+    @Test
+    public abstract void getDoubleAt();
+
+    @Test
+    public abstract void getLongAt();
+
+    @Test
+    public abstract void getStringAt();
+
+    @Test
+    public abstract void getAnyAt();
+
+    @Test
+    public abstract void testToString();
+
+    @Test
+    public abstract void toPrettyString();
+
+    @Test
+    public abstract void asString();
+
+    @Test
+    public abstract void asPrettyString();
+
+    @Test
+    public abstract void asPrettyStringWithDepth();
+
+    @Test
+    public abstract void asPrettyStringWithDepthAndIndent();
+
+    @Test
+    public abstract void testEquals();
+
+    @Test
+    public abstract void getHashCode();
 }
