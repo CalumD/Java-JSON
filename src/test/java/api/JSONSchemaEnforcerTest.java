6e84707d28ba473105edf275c61f11721800850b
Calum <CalumD3@hotmail.com>
2020-07-29 01:28:35 +0100

    Prep some schema validation re-writes

---
+++ src/test/java/api/JSONSchemaEnforcerTest.java
@@ -0,0 +1,49 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class JSONSchemaEnforcerTest {
+
+    @Test
+    public void schemaMustBeAnObject() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void noSchemaProblemShouldBeTrueWhenStrict() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void noSchemaProblemShouldBeTrueWhenNOTStrictToo() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void schemaExceptionShouldBeReturnedAsFalse() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void schemaExceptionShouldBeThrownAsException() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void neitherSchemaNorJSONShouldBeNulls() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void baseObjectShouldAcceptUnrecognisedStrings() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void baseObjectMUSTHaveCertainProperties() {
+        fail("Not yet implemented");
+    }
+
+}
